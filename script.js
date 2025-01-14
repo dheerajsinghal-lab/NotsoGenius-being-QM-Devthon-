340b0934bf6ba6a082c3aa794e6d9e3ff982fcbc
dheerajsinghal-lab <singhal.dheeraj2020@gmail.com>
2025-01-14 14:58:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,92 @@
 const questions = [
     {
-        question: "Which is the largest animal in the world?",
+        question: "What is the name of the court poet of King Harshavardhana?",
         answers: [
-            { text: "Shark", correct: false },
-            { text: "Blue Whale", correct: true },
-            { text: "Elephant", correct: false },
-            { text: "Giraffe", correct: false },
+            { text: "Tulsidas", correct: false },
+            { text: "Banabhatta", correct: true },
+            { text: "Surdas", correct: false },
+            { text: "Raskhan", correct: false }
         ]
     },
     {
-        question: "Which is the smallest country in the world?",
+        question: "Which emperor wrote the play 'Nagananda' in Sanskrit language?",
         answers: [
-            { text: "Vatican City", correct: true },
-            { text: "Bhutan", correct: false },
-            { text: "Nepal", correct: false },
-            { text: "Sri Lanka", correct: false },
+            { text: "Prabhakaravardhana", correct: false },
+            { text: "Harshavardhana", correct: true },
+            { text: "Chandragupta II", correct: false },
+            { text: "Bindusara", correct: false }
+        ]
+    },
+     {
+        question: "Takshashila University was located between which two rivers?",
+        answers: [
+            { text: "Indus and Jhelum", correct: true },
+            { text: "Jhelum and Ravi", correct: false },
+            { text: "Beas and Indus", correct: false },
+            { text: "Satluj and Indus", correct: false }
+         ]
+     },
+    {
+         question: "Who is the most prominent god of 'Rig Veda'?",
+         answers: [
+             { text: "Indra", correct: true },
+             { text: "Agni", correct: false },
+             { text: "Surya", correct: false },
+             { text: "Varuna", correct: false }
+         ]
+     },
+    {
+         question: "Jama Masjid of Delhi was built in which century?",
+         answers: [
+             { text: "15th", correct: false },
+             { text: "16th", correct: false },
+             { text: "17th", correct: true },
+             { text: "18th", correct: false }
+         ]
+    },
+      {
+        question: "In which year did the Portuguese captured Goa from Bijapur?",
+         answers: [
+             { text: "1498", correct: false },
+              { text: "1510", correct: true },
+              { text: "1516", correct: false },
+             { text: "1569", correct: false }
+         ]
+     },
+    {
+        question: "Who among the following were the pioneers in opening oceanic trade with India?",
+         answers: [
+             { text: "Dutch", correct: false },
+              { text: "English", correct: false },
+             { text: "French", correct: false },
+             { text: "Portuguese", correct: true }
+         ]
+    },
+    {
+        question: "The tax collected by Marathas was known as",
+         answers: [
+             { text: "Chauth", correct: true },
+              { text: "Pilgrim Tax", correct: false },
+              { text: "Jazia", correct: false },
+              { text: "Charai", correct: false }
         ]
     },
     {
-        question: "Which is the largest desert in the world?",
-        answers: [
-            { text: "Kalahari", correct: false },
-            { text: "Gobi", correct: false },
-             { text: "Sahara", correct: false },
-            { text: "Antarctica", correct: true },
+        question: "At which of the following places did Hyder Ali built a modern arsenal?",
+         answers: [
+             { text: "Dindigul", correct: true },
+             { text: "Mysore", correct: false },
+             { text: "Srirangappattanam", correct: false },
+              { text: "Arcot", correct: false }
         ]
     },
-    {
-        question: "Which is the smallest continent in the world?",
-        answers: [
-            { text: "Asia", correct: false },
-            { text: "Australia", correct: true },
-            { text: "Arctic", correct: false },
-            { text: "Africa", correct: false },
+   {
+        question: "Who planted the 'Tree of Liberty' at Srirangapatnam?",
+         answers: [
+             { text: "Chikka Krishna Raj", correct: false },
+             { text: "Devraj", correct: false },
+             { text: "Hyder Ali", correct: false },
+              { text: "Tipu Sultan", correct: true }
         ]
     }
 ];
@@ -43,19 +97,25 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+let selectedQuestions = [];
 
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
+    // Shuffle the questions array
+    const shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    // Select the first 5 questions
+    selectedQuestions = shuffledQuestions.slice(0, 5);
     showQuestion();
 }
 
+
 function showQuestion() {
     resetState();
-    let currentQuestion = questions[currentQuestionIndex];
+    let currentQuestion = selectedQuestions[currentQuestionIndex];
     let questionNumber = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+     questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
@@ -97,7 +157,7 @@ function selectAnswer(e){
 
 function handleNextButton(){
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
+    if(currentQuestionIndex < selectedQuestions.length){
         showQuestion();
     } else {
         showScore();
@@ -106,14 +166,14 @@ function handleNextButton(){
 
 function showScore(){
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    questionElement.innerHTML = `You scored ${score} out of ${selectedQuestions.length}!`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
 
 
 nextButton.addEventListener("click", ()=>{
-    if(currentQuestionIndex < questions.length){
+    if(currentQuestionIndex < selectedQuestions.length){
       handleNextButton();
     } else {
         startQuiz();
